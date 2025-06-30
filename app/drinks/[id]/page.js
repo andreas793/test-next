9e311cc04eaf2ba86c775f43c456135e407e32f4
andreas793 <andreas7_93@mail.ru>
2025-06-30 16:56:13 +0300

    22, 23

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,8 +1,26 @@
-const SingleDrinkPage = ({params}) => {
-    console.log(params.id);
+import Link from "next/link";
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+const getSingleDrink = async (id) => {
+    const res = await fetch(`${url}${id}`);
+    if(!res.ok){
+        throw new Error('Failed to fetch a drink...');
+    }
+    return res.json();
+}
+
+
+
+const SingleDrinkPage = async ({params}) => {
+    const data = await getSingleDrink(params.id);
+    const title = data?.drinks[0]?.strDrink;
+    const imgSrc = data?.drinks[0]?.strDrinkThumb;
     return (
         <div>
-            SingleDrinkPage
+            <Link className="btn btn-primary mt-8 mb-12" href="/drinks">Back to drinks</Link>
+            <h3 className='text-4xl mb-8'>{title}</h3>
+            <div><img src={imgSrc} alt=""/></div>
         </div>
     );
 };
