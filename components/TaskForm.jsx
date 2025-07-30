ed919cf4cc0e7e1f8ec21ddb51ae25b38abb56cd
andreas793 <andreas7_93@mail.ru>
2025-07-30 17:18:20 +0300

    32,33 - Create Tasks

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,5 +1,31 @@
+import prisma from "@/utils/db";
+import {revalidatePath} from "next/cache";
+
+const createTask = async (formData) => {
+    'use server'
+    const content = formData.get('content');
+    await prisma.task.create({
+        data: {
+            content,
+        }
+    });
+    revalidatePath('/tasks ')
+}
+
 const TaskForm = () => {
-    return <div>TaskForm</div>
+    return <form action={createTask}>
+        <div className="flex join w-full border">
+            <input type="text"
+                   className="input input-bordered w-full"
+                   placeholder="type here"
+                   name='content'
+                   required
+            />
+            <button type="submit" className=" btn btn-primary rounded-xs p-1 bg-blue-500">
+                create
+            </button>
+        </div>
+    </form>
 }
 
 export default TaskForm;
